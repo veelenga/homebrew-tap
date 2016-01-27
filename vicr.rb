817e80ae12c907c6463ded7ac050cdfa41af9c7f
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-01-27 11:58:35 +0200

    Vicr v0.3.0

--- vicr.rb
+++ vicr.rb
@@ -1,9 +1,9 @@
 class Vicr < Formula
   desc "Vim-like Interactive CRystal"
   homepage "https://github.com/veelenga/vicr"
-  url "https://github.com/veelenga/vicr/archive/v0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "d33b89a8243ea0d277c93df5d6176dccec914079788a9b6d2eb71f71158d6595"
+  url "https://github.com/veelenga/vicr/archive/v0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "1407fddd6ef5ebd17ca45d451fb627dde19e5b756a3063787df4283a5cdbd8b2"
   head "https://github.com/veelenga/vicr.git"
 
   depends_on "crystal-lang" => :build
