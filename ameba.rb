4e70bd3af52abe8673ce74e3b2672e54c928d913
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-12-06 17:22:15 +0200

    Ameba v0.3.0

--- ameba.rb
+++ ameba.rb
@@ -1,9 +1,9 @@
 class Ameba < Formula
   desc "Code style linter for Crystal"
   homepage "https://github.com/veelenga/ameba"
-  url "https://github.com/veelenga/ameba/archive/v0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "152c7ea139f0b24a857d02208fcdfd288db8cf79034852ab03283613ce4ae260"
+  url "https://github.com/veelenga/ameba/archive/v0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "9b3eac9e404b0e543e490ffbb8ac5a6b5b1fef640856113c5ad8f08b804845a3"
   head "https://github.com/veelenga/ameba.git"
 
   depends_on "crystal-lang" => :build
