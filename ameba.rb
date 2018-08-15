825e0c1de22d84d095e8d2ceb35d89bffcfbfe6f
Vitalii Elenhaupt <velenhaupt@gmail.com>
2018-08-15 20:11:42 +0300

    Ameba v0.8.0

--- ameba.rb
+++ ameba.rb
@@ -1,9 +1,9 @@
 class Ameba < Formula
   desc "A static code analysis tool for Crystal"
   homepage "https://github.com/veelenga/ameba"
-  url "https://github.com/veelenga/ameba/archive/v0.7.0.tar.gz"
-  version "0.7.0"
-  sha256 "035d4d684021ce7cef6d1345152168eacd69f2a97c16ff0417c1cd2992a08634"
+  url "https://github.com/veelenga/ameba/archive/v0.8.0.tar.gz"
+  version "0.8.0"
+  sha256 "e5e2135a5d9bca3cbcbb31b73eede54d9edd88fa43eac7780ff05cb5ec732341"
   head "https://github.com/veelenga/ameba.git"
 
   depends_on "crystal-lang" => :build
