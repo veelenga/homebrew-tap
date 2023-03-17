b2189c8347015e1eb0d676dfbf191ab440e6d856
Vitalii Elenhaupt <velenhaupt@gmail.com>
2023-03-17 10:37:14 +0200

    Bump ameba v1.4.3

--- ameba.rb
+++ ameba.rb
@@ -1,9 +1,9 @@
 class Ameba < Formula
   desc 'A static code analysis tool for Crystal'
   homepage 'https://github.com/veelenga/ameba'
-  url 'https://github.com/veelenga/ameba/archive/v1.4.2.tar.gz'
-  version '1.4.2'
-  sha256 '844c87f6e4d8ddf2356079183b3b289b88dff239c669d0d1b6a4db2a425ebd7e'
+  url 'https://github.com/veelenga/ameba/archive/v1.4.3.tar.gz'
+  version '1.4.3'
+  sha256 'c42209a2e39f2f954776befc2a9ff441250390df1bcf8368a17150270ccf5c27'
   head 'https://github.com/veelenga/ameba.git'
 
   depends_on 'crystal-lang'
