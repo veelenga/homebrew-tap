e963a6dd0b1eb6ceeb96e5d7039429995d4aaa68
Vitalii Elenhaupt <3624712+veelenga@users.noreply.github.com>
2026-08-20 22:11:01 +0200

    igut 0.1.0-beta

--- igut.rb
+++ igut.rb
@@ -1,29 +1,30 @@
 class Igut < Formula
   desc "Browse, preview, and copy media from the Insta360 GO Ultra over WiFi"
   homepage "https://github.com/veelenga/insta360-go-ultra-transfer"
-  url "https://github.com/veelenga/insta360-go-ultra-transfer/releases/download/v0.1.0/insta360-go-ultra-transfer-0.1.0.tar.gz"
-  sha256 "e05a52cabd8ec408dd6cd53dce08e0a522574aaf0b375148fb5c9312a5715ccb"
+  url "https://github.com/veelenga/insta360-go-ultra-transfer/releases/download/v0.1.0-beta/insta360-go-ultra-transfer-0.1.0-beta.tar.gz"
+  sha256 "6821bdadad4678570f159d673706e78c6ad6365c6782785fcb28f3ebc1b4a89d"
   license "MIT"
 
   depends_on "python@3.13"
 
   def install
-    libexec.install "server.py", "go_ultra.py", "static"
+    libexec.install "cli.py", "server.py", "go_ultra.py", "transfer.py", "static"
     (bin/"igut").write <<~SH
       #!/bin/bash
-      exec "#{Formula["python@3.13"].opt_bin}/python3.13" "#{libexec}/server.py" "$@"
+      exec "#{Formula["python@3.13"].opt_bin}/python3.13" "#{libexec}/cli.py" "$@"
     SH
   end
 
   def caveats
     <<~TEXT
-      Connect to the camera's WiFi hotspot, run:
-        igut
-      then open http://127.0.0.1:8765
+      Connect to the camera's WiFi hotspot, then:
+        igut server        # web UI at http://127.0.0.1:8765
+        igut ls            # list files on the camera
+        igut download ...  # copy files to a local folder
     TEXT
   end
 
   test do
-    assert_path_exists libexec/"server.py"
+    assert_match "igut", shell_output("#{bin}/igut --help")
   end
 end
